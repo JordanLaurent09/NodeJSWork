f07086e43f4fa441fe17224e8da08cb76be48049
Сергей Ш <128598320+JordanLaurent09@users.noreply.github.com>
2024-10-16 18:40:27 +0200

    Lesson 3

---
+++ Lesson3. Установка сторонних модулей/src/index.js
@@ -0,0 +1,51 @@
+const moment = require('moment');
+
+/*let now = moment();
+
+console.log(now.format());*/
+
+/* const someData = moment("2024-08-15");
+console.log(someData.format("DD MM YYYY")); */
+
+
+/* const now = moment();
+console.log(now.format("DD-MM-YYYY"));
+console.log(now.format("MMMM Do YYYY, h:mm:ss a"));
+console.log(now.format("dddd"));
+console.log(now.format("MMM Do YY")); */
+
+/* const nextWeek = moment().add(7, "days")
+console.log(nextWeek.format("DD.MM.YYYY")); */
+
+/* const nextWeek = moment().subtract(7, "days")
+console.log(nextWeek.format("DD.MM.YYYY")); */
+
+/*const example = moment().endOf("month")
+console.log(example.format("DD.MM.YYYY"));*/
+
+/* const dateValidate = moment("2024-10-21", "YYYY-MM-DD", true);
+console.log(dateValidate); */
+
+/* const first = moment("2024-05-15");
+const second = moment("2023-08-22");
+
+console.log(first.isAfter(second)); // После
+console.log(first.isBefore(second)); // Перед
+console.log(first.isSame(second)); // Одинаковы ли */
+
+/* const first = moment("2024-05-15");
+const second = moment("2023-08-22");
+
+const difference = first.diff(second, "days");
+console.log(difference); */
+
+
+/*const now = moment().locale('ch');
+console.log(now);*/
+
+/* const now = moment();
+
+console.log(now.format("L"));
+console.log(now.format("LL"));
+console.log(now.format("LLL"));
+console.log(now.format("LLLL")); */
